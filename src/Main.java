dba005cbcccf7937247224de9f4af15f7fc5c093
eremi <ereminr83@mail.ru>
2023-10-01 22:06:19 +0700

    1 commit

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+
+//Необходимо реализовать следующий метод:
+//
+//        Получаем на входе массив чисел.
+//        Все четные числа увеличиваем на единицу.
+//        Возвращаем кусок списка с 3-го по 7-й элемент.
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int start = 3, end = 7;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                arr[i] += 1;
+
+            }
+            }
+        int[] arr2 = Arrays.copyOfRange(arr, start, end + 1);
+        System.out.println(Arrays.toString(arr2));
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
